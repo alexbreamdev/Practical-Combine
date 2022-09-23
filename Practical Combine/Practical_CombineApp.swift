68bd3f193cfac44062420a8c59812c49f83c3fff
alexbreamdev <kortes39dev@gmail.com>
2022-09-23 18:40:15 +0300

    Initial Commit

---
+++ Practical Combine/Practical_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practical_CombineApp.swift
+//  Practical Combine
+//
+//  Created by Oleksii Leshchenko on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Practical_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
